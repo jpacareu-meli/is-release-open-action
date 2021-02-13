3a52b7fe226cb519e56f4da9e6483559fdf5fb39
Javier Pacareu <javier.pacareu@mercadolibre.cl>
2021-02-13 18:54:41 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,30 +4,25 @@ import api  from 'octonode';
 
 const client = api.client(process.env.GITHUB_TOKEN);
 
-async function getOpenPullRequests() {
-  const repo = client.repo(github.context.payload.repository.full_name);
-  const result = await repo.prsAsync({ per_page: 100, state: "open" });
-  return result[0];
+const hasOpenRelease = async () => {
+  console.log(`REPO:"${github.repository}" and PR:${github.event.number}`);
+  const result = await client.get(`/repos/${github.repository}/pulls?per_page=100&state=open`);
+  console.log(`Length: ${result.length}`);
+  return !!result.findIndex(el => /release\//.test(el.head.ref));
 }
 
-async function addWarningComment() {
-  // get pr id
-  if(github.context.eventName === 'pull_request') {
-    var ghpr = client.pr(github.context.payload.repository.full_name, 123);
-    // ghpr.createCommentAs({
-    //   body: 'my comment',
-    //   commit_id: '8cde3b6c5be2c3067cd87ee4117c0f65e30f3e1f', // needed to comment on current time in PR
-    // });
-  }
+const addWarningComment = async () => {
+  await client.post(`/repos/${github.repository}/pulls/${github.event.number}/comments`, {
+    body: 'MOSCA OPEN PULL REQUEST'
+  });
 }
 
 async function run() {
   try {
-    const prs = await getOpenPullRequests()
-    const prsJSON = JSON.stringify(prs, undefined, 2);
-
-    console.log(`The OPEN PRs are: ${prsJSON}`);
-    console.log(`Context: ${JSON.stringify(github.context, undefined, 2)}`);
+    const openRelease = await hasOpenRelease()
+    if(openRelease) {
+      await addWarningComment()
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
